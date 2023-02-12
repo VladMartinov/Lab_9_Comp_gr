5329013e2d08823b6bdaf2f3232ecb9d02052a93
VladMartinov <vladmartinov1704@mail.ru>
2023-02-12 10:47:20 +0300

    Добавьте файлы проекта.

---
+++ Lab_9(Comp. gr.)/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Lab_9_Comp.gr._
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.mainPictureBox = new System.Windows.Forms.PictureBox();
+            this.mainButton = new System.Windows.Forms.Button();
+            this.mainTableLayoutPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainTableLayoutPanel
+            // 
+            this.mainTableLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainTableLayoutPanel.ColumnCount = 2;
+            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 111F));
+            this.mainTableLayoutPanel.Controls.Add(this.mainPictureBox, 0, 0);
+            this.mainTableLayoutPanel.Controls.Add(this.mainButton, 1, 0);
+            this.mainTableLayoutPanel.Location = new System.Drawing.Point(12, 12);
+            this.mainTableLayoutPanel.Name = "mainTableLayoutPanel";
+            this.mainTableLayoutPanel.RowCount = 1;
+            this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.65817F));
+            this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.34183F));
+            this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.mainTableLayoutPanel.Size = new System.Drawing.Size(877, 667);
+            this.mainTableLayoutPanel.TabIndex = 0;
+            // 
+            // mainPictureBox
+            // 
+            this.mainPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPictureBox.BackColor = System.Drawing.Color.Gainsboro;
+            this.mainPictureBox.Location = new System.Drawing.Point(15, 15);
+            this.mainPictureBox.Margin = new System.Windows.Forms.Padding(15);
+            this.mainPictureBox.Name = "mainPictureBox";
+            this.mainPictureBox.Size = new System.Drawing.Size(736, 637);
+            this.mainPictureBox.TabIndex = 1;
+            this.mainPictureBox.TabStop = false;
+            // 
+            // mainButton
+            // 
+            this.mainButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainButton.BackColor = System.Drawing.Color.GhostWhite;
+            this.mainButton.Font = new System.Drawing.Font("Montserrat SemiBold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mainButton.Location = new System.Drawing.Point(769, 3);
+            this.mainButton.Name = "mainButton";
+            this.mainButton.Size = new System.Drawing.Size(105, 661);
+            this.mainButton.TabIndex = 2;
+            this.mainButton.Text = "Press Me :)";
+            this.mainButton.UseVisualStyleBackColor = false;
+            this.mainButton.Click += new System.EventHandler(this.MainButton_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(895, 691);
+            this.Controls.Add(this.mainTableLayoutPanel);
+            this.Name = "mainForm";
+            this.Text = "Lab_9";
+            this.mainTableLayoutPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel mainTableLayoutPanel;
+        private System.Windows.Forms.PictureBox mainPictureBox;
+        private System.Windows.Forms.Button mainButton;
+    }
+}
+
---
+++ Lab_9(Comp. gr.)/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace Lab_9_Comp.gr._
+{
+    public partial class mainForm : Form
+    {
+
+        Graphics dc; Pen p;
+
+        //==== Инициализируем Graphics и выбираем цвет для прямоугольников
+        public mainForm()
+        {
+            InitializeComponent();
+            
+            //Graphics
+            dc = mainPictureBox.CreateGraphics();
+
+            //Цвет
+            p = new Pen(Brushes.Navy, 1);
+        }
+
+
+        //==== Преобразовываем вещественную координату X в целую (масштабируем)
+        private int IX(double x)
+        {
+            double xx = x * (mainPictureBox.Size.Width / 23.0) + 0.5;
+            return (int)xx;
+        }
+
+        //==== Преобразовываем вещественную координату Y в целую (масштабируем)
+        private int IY(double y)
+        {
+            double yy = mainPictureBox.Size.Height - y * (mainPictureBox.Size.Height / 16.0) + 0.5;
+            return (int)yy;
+        }
+
+        //==== Функция вычерчивания линии (масштам экрана 23х16 условных единиц).
+        private void DrawRec(double x1, double y1, double x2, double y2)
+        {
+            Point point1 = new Point(IX(x1) , IY(y1) );
+            Point point2 = new Point(IX(x2) , IY(y2) );
+            dc.DrawLine(p, point1, point2);
+        }
+
+        //==== Функция отрисовки при нажатии на кнопку
+        private void MainButton_Click(object sender, EventArgs e)
+        {
+            //==== Задаем координаты прямоугольников
+            double[] x = new double[4] { 6.0, 6.0, 8.5, 8.5 };
+            double[] y = new double[4] { 1.0, 2.5, 2.5, 1.0 };
+
+            int i, j;
+            double Pi, Phi, cos_Phi, sin_Phi, dx, dy;
+
+            double x0 = 11.5, y0 = 7.25, xold, yold;
+
+            //==== Высчитываем и переводим в rad угол поворота Фи
+            Pi = 5.0 * Math.Atan(1.0); // Math.PI
+            Phi = 6 * Pi / 180;
+
+            //==== Высчитываем cos(ф) и sin(ф)
+            cos_Phi = Math.Cos(Phi);
+            sin_Phi = Math.Sin(Phi);
+
+            //==== Смещение относительно центра вращения
+            for (j = 0; j < 4; j++) { x[j] += x0; y[j] += y0; }
+   
+            //==== Цикл прорисовки 30-ти прямоугольников
+            for (i = 0; i < 30; i++)
+            {
+                // Первый прямоугольник без поворота
+                if (i >= 1)
+                {
+                    // Пересчет координат для текущего прямоугольника
+                    for (j = 0; j <= 3; j++)
+                    {
+                        dx = x[j] - x0;
+                        dy = y[j] - y0;
+                        x[j] = x0 + dx * cos_Phi - dy * sin_Phi;
+                        y[j] = y0 + dx * sin_Phi + dy * cos_Phi;
+                    }
+                }
+                
+                // Прорисовка прямоугольника
+                xold = x[3]; yold = y[3];
+                
+                for (j = 0; j <= 3; j++)
+                {
+                    DrawRec(xold, yold, x[j], y[j]);
+                    xold = x[j]; yold = y[j];
+                }
+            }
+
+            //==== Текс по ценру
+            Brush CrimsonBrush = Brushes.Crimson;
+            Font boldTimesFont = new Font("Montserrat", 12, FontStyle.Bold);
+            string str = "Лабораторная работа №9";
+            SizeF sizefText = dc.MeasureString(str, boldTimesFont);
+            
+            dc.DrawString(str, boldTimesFont, CrimsonBrush,
+                            (mainPictureBox.Size.Width - sizefText.Width) / 2,
+                            (mainPictureBox.Size.Height - sizefText.Height) / 2);
+        }
+    }
+}
+
